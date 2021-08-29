0a36eb41169c76ef201948c7274a65e1c68fb413
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-28 23:33:23 -0300

    test(GetBalanceUseCase): ensure that the user balance is returned properly

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateStatementError } from "./CreateStatementError";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
@@ -11,10 +12,6 @@ const user = {
   email: "Email",
   password: "Password"
 };
-enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
-}
 
 describe("Create Statement", () => {
   beforeEach(() => {
---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+
+let getBalanceUseCase: GetBalanceUseCase;
+let statementsRepositoryInMemory: InMemoryStatementsRepository;
+let usersRepositoryInMemory: InMemoryUsersRepository;
+const user = {
+  name: "Name",
+  email: "Email",
+  password: "Password"
+};
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+describe("Get Balances", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    statementsRepositoryInMemory = new InMemoryStatementsRepository();
+    getBalanceUseCase = new GetBalanceUseCase(
+      statementsRepositoryInMemory,
+      usersRepositoryInMemory
+    );
+  });
+  it("should be able to get the user balance", async () => {
+    const userCreated = await usersRepositoryInMemory.create(user)
+    const statement = {
+      type: OperationType.DEPOSIT,
+      amount:10,
+      description:'Description',
+      user_id: userCreated.id as string
+    }
+
+    await statementsRepositoryInMemory.create(statement);
+    const balance = await getBalanceUseCase.execute({user_id:userCreated.id as string})
+
+    expect(balance.balance).toBe(statement.amount)
+  });
+});

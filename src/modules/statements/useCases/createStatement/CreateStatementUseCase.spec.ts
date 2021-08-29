8ef70de8491d5675061b58c56431892af6cda357
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-28 23:14:04 -0300

    test(CreateStatementUseCase): ensure that new withdraw statement are created

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { User } from "../../../users/entities/User";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
@@ -5,7 +6,11 @@ import { CreateStatementUseCase } from "./CreateStatementUseCase";
 let createStatementUseCase: CreateStatementUseCase;
 let statementsRepositoryInMemory: InMemoryStatementsRepository;
 let usersRepositoryInMemory: InMemoryUsersRepository;
-
+const user = {
+  name: "Name",
+  email: "Email",
+  password: "Password"
+};
 enum OperationType {
   DEPOSIT = 'deposit',
   WITHDRAW = 'withdraw',
@@ -21,15 +26,21 @@ describe("Create Statement", () => {
     );
   });
   it("should be able to create a new deposit statement", async () => {
-    const user = {
-      name: "Name",
-      email: "Email",
-      password: "Password"
-    }
     const userCreated = await usersRepositoryInMemory.create(user)
 
     const statementCreated = await createStatementUseCase.execute(
       { type:OperationType.DEPOSIT, amount:10, description:'Description', user_id: userCreated.id as string}
+    );
+    expect(statementCreated).toHaveProperty("id");
+  });
+  it("should be able to create a new withdraw statement", async () => {
+    const userCreated = await usersRepositoryInMemory.create(user)
+
+    await createStatementUseCase.execute(
+      { type:OperationType.DEPOSIT, amount:10, description:'Description', user_id: userCreated.id as string}
+    );
+    const statementCreated = await createStatementUseCase.execute(
+      { type:OperationType.WITHDRAW, amount:10, description:'Description', user_id: userCreated.id as string}
     );
     expect(statementCreated).toHaveProperty("id");
   });
